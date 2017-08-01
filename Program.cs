d4ac551555b884c858ca61bceed2aca89e62de84
John Colley <webjocky@gmail.com>
2017-08-01 14:25:10 -0700

    added boxing_unboxing assignment

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> things = new List<object>();
+            things.Add(7);
+            things.Add(28);
+            things.Add(-1);
+            things.Add(true);
+            things.Add("chair");
+
+            int sum = 0;
+            for(int i = 0;i<things.Count;i++){
+                Console.WriteLine(things[i]);
+                if(things[i] is int){
+                    sum = sum + (int)things[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
